ec4806a52ffbb0982734e0b07f2d61d1fc28beb4
Pyae Sone <pyaesone.perfect2014@gmail.com>
2023-02-12 18:26:25 +0630

    Add the functionality to check if game

--- script/script.js
+++ script/script.js
@@ -15,21 +15,81 @@ function Player(Symbol) {
 }
 
 const Gameboard = (() => {
-  const gameboard = [1, 1, 1, 1, 1, 1, 1, 1, 1];
-  (function renderGameboard() {
+  const gameboard = new Array(9);
+  function renderGameboard() {
     const gameBoard = document.getElementById("gameboard");
     for (let i = 0; i < gameboard.length; i++) {
       const square = document.createElement("div");
       square.id = `square${i}`;
       square.className = "square";
       const squareContent = document.createElement("p");
-      squareContent.innerText = gameboard[i];
+      squareContent.innerText = `${gameboard[i] ? gameboard[i] : ""}`;
       square.appendChild(squareContent);
       gameBoard.appendChild(square);
     }
-  })();
-})();
+  }
+  renderGameboard();
+  function clearGameboard() {
+    const gameBoard = document.getElementById("gameboard");
+    while (gameBoard.lastChild) {
+      gameBoard.removeChild(gameBoard.lastChild);
+    }
+  }
+  const mark = (x, y) => {
+    gameboard[x] = y;
+    clearGameboard();
+    renderGameboard();
+  };
 
-const ScoreDisplayer = (() => {
-  const displayScore = (x) => x.score;
+  const checkGame = () => {
+    if (gameboard[0] && gameboard[1] && gameboard[2]) {
+      if (gameboard[0] === gameboard[1] && gameboard[0] === gameboard[2]) {
+        return true;
+      }
+      return false;
+    }
+    if (gameboard[3] && gameboard[4] && gameboard[5]) {
+      if (gameboard[3] === gameboard[4] && gameboard[3] === gameboard[5]) {
+        return true;
+      }
+      return false;
+    }
+    if (gameboard[6] && gameboard[7] && gameboard[8]) {
+      if (gameboard[6] === gameboard[7] && gameboard[6] === gameboard[8]) {
+        return true;
+      }
+      return false;
+    }
+    if (gameboard[0] && gameboard[3] && gameboard[6]) {
+      if (gameboard[0] === gameboard[3] && gameboard[0] === gameboard[6]) {
+        return true;
+      }
+      return false;
+    }
+    if (gameboard[1] && gameboard[4] && gameboard[7]) {
+      if (gameboard[1] === gameboard[4] && gameboard[1] === gameboard[7]) {
+        return true;
+      }
+      return false;
+    }
+    if (gameboard[2] && gameboard[5] && gameboard[8]) {
+      if (gameboard[2] === gameboard[5] && gameboard[2] === gameboard[8]) {
+        return true;
+      }
+      return false;
+    }
+    if (gameboard[0] && gameboard[4] && gameboard[8]) {
+      if (gameboard[0] === gameboard[4] && gameboard[0] === gameboard[8]) {
+        return true;
+      }
+      return false;
+    }
+    if (gameboard[2] && gameboard[4] && gameboard[6]) {
+      if (gameboard[2] === gameboard[4] && gameboard[2] === gameboard[6]) {
+        return true;
+      }
+      return false;
+    }
+  };
+  return { mark, checkGame };
 })();
